b8e1e90bb39528f90519773bd5bf0fc4dc174f9a
bojantanasic <bojan.tanasic@gmail.com>
2026-01-11 20:12:23 +0100

    sched: Fix building error

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -844,10 +844,26 @@ static void sugov_walt_adjust(struct sugov_cpu *sg_cpu, unsigned long *util,
  */
 static inline void ignore_dl_rate_limit(struct sugov_cpu *sg_cpu, struct sugov_policy *sg_policy)
 {
-
+#ifdef OPLUS_FEATURE_POWER_CPUFREQ
+	if (cpu_bw_dl(cpu_rq(sg_cpu->cpu)) > sg_cpu->bw_dl) {
+		sg_policy->limits_changed = true;
+		sg_policy->after_limits_changed = true;
+	}
+#else
 	if (cpu_bw_dl(cpu_rq(sg_cpu->cpu)) > sg_cpu->bw_dl)
-		WRITE_ONCE(sg_policy->limits_changed, true);
+		sg_policy->limits_changed = true;
+#endif
+}
 
+static inline unsigned long target_util(struct sugov_policy *sg_policy,
+				  unsigned int freq)
+{
+	unsigned long util;
+
+	util = freq_to_util(sg_policy, freq);
+	util = mult_frac(util, TARGET_LOAD, 100);
+	WRITE_ONCE(sg_policy->limits_changed, true);
+	return util;
 }
 
 static void sugov_update_single(struct update_util_data *hook, u64 time,
@@ -1782,6 +1798,24 @@ static void sugov_limits(struct cpufreq_policy *policy)
 		raw_spin_unlock_irqrestore(&sg_policy->update_lock, flags);
 	}
 
+#ifdef OPLUS_FEATURE_POWER_CPUFREQ
+	if (policy->min == policy->cpuinfo.max_freq &&
+	    policy->min > sg_policy->min_freq) {
+		sg_policy->start_time = ktime_get();
+		sg_policy->freq_locked = true;
+	} else if (sg_policy->freq_locked && policy->min < policy->max) {
+		now = ktime_get();
+		delta = ktime_to_ns(ktime_sub(now, sg_policy->start_time));
+		if (delta >= 8 * NSEC_PER_SEC)
+			pr_warn("policy%d's freq locked at max_freq for %lld(ns)",
+				cpumask_first(policy->related_cpus), delta);
+		sg_policy->freq_locked = false;
+	}
+	sg_policy->min_freq = policy->min;
+	sg_policy->after_limits_changed = true;
+#endif
+
+	sg_policy->limits_changed = true;
 	/*
 	 * The limits_changed update below must take place before the updates
 	 * of policy limits in cpufreq_set_policy() or a policy limits update
